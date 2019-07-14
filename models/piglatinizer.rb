c84c54b0c9577847833e3076984b4029d5f1db0f
elijahmm17 <elijahmm17@gmail.com>
2019-07-14 03:59:40 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,13 +5,13 @@ class PigLatinizer
     words = phrase.split(" ")
     words.map do |word|
       if word[0].match(/[aeiouAEIOU]/)
-        pl_word = word + 'way '
+        pl_word = word + 'way'
       else
         letters = word.chars 
         first_vowel = letters.find_index {|l| l.match(/[aeiouAEIOU]/)}
         syl1 = word[first_vowel, word.length - 1]
         syl2 = word[0, first_vowel]
-        pl_word = syl1 + syl2 + 'ay '
+        pl_word = syl1 + syl2 + 'ay'
       end
       converted << pl_word
     end
