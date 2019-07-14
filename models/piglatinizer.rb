a3d63b911484ca93189c681a0540d2af9a2fcc5c
elijahmm17 <elijahmm17@gmail.com>
2019-07-14 03:37:13 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,22 +1,21 @@
 class PigLatinizer 
   
   def piglatinize(phrase) 
-    "hello world"
-    # converted = ''
-    # words = phrase.split(" ")
-    # words.each do |word|
-    #   if word[0].match(/[aeiouAEIOU]/)
-    #     result = word + 'way '
-    #   else 
-    #     letters = word.chars 
-    #     first_vowel = letters.find_index {|l| l.match(/[aeiouAEIOU]  /)}
-    #     syl1 = word[first_vowel, word.length - 1]
-    #     syl2 = word[0, first_vowel]
-    #     result = syl1 + syl2 + 'ay '
-    #   end
-    #   converted += result
-    # end
-    # converted
+    converted = ''
+    words = phrase.split(" ")
+    words.each do |word|
+      if word[0].match(/[aeiouAEIOU]/)
+        result = word + 'way '
+      else 
+        letters = word.chars 
+        first_vowel = letters.find_index {|l| l.match(/[aeiouAEIOU]  /)}
+        syl1 = word[first_vowel, word.length - 1]
+        syl2 = word[0, first_vowel]
+        result = syl1 + syl2 + 'ay '
+      end
+      converted += result
+    end
+    converted
   end
   
 end 
